afdecc39ea822d337621f2119cf6cb974d03fbb5
SRGmeedia <srgmeedia@gmail.com>
2025-01-13 20:30:52 +0200

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "245129130"
+    serviceWorkerVersion: "3148130097"
   }
 });
